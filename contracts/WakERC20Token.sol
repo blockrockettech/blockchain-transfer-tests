225d05eb7430cbdb72759a45e92a086fd95a1978
Andy Gray <andy@whatsthescore.co.uk>
2018-08-08 13:55:22 +0100

    report gas

--- contracts/WakERC20Token.sol
+++ contracts/WakERC20Token.sol
@@ -5,10 +5,22 @@ import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 
 
 contract WakERC20Token is StandardToken, DetailedERC20 {
+
     constructor(address _initialAccount, uint256 _initialBalance)
     DetailedERC20("Wakelet Token", "WAK", 18)
     public {
         balances[_initialAccount] = _initialBalance;
         totalSupply_ = _initialBalance;
+    }
+
+    /**
+    * @dev Transfer token to multiple addresses
+    * @param _addresses The addresses to transfer to.
+    * @param _values The amount values to be transferred.
+    */
+    function batchTransfer(address[] _addresses, uint256[] _values) public returns (bool) {
+        for (uint i = 0; i < _addresses.length; i++) {
+            transfer(_addresses[i], _values[i]);
+        }
     }
 }
--- test/WakERC20Token.test.js
+++ test/WakERC20Token.test.js
@@ -1,172 +1,169 @@
-const assertRevert = require('./helpers/assertRevert');
+
+const weiToEther = require('./helpers/weiToEther');
 
 const WakERC20Token = artifacts.require('WakERC20Token');
 
-contract('WakERC20Token', function ([_, owner, recipient, anotherAccount]) {
-  const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const AsciiTable = require('ascii-table')
 
-  beforeEach(async function () {
-    this.token = await WakERC20Token.new(owner, 100);
-  });
+contract('WakERC20Token', function ([
+                                        _,
+                                        owner,
+                                        recipient,
+                                        account1,
+                                        account2,
+                                        account3,
+                                        account4,
+                                        account5,
+                                        account6,
+                                        account7,
+                                        account8,
+                                        account9,
+                                        account10
+                                    ]) {
 
-  describe('total supply', function () {
-    it('returns the total amount of tokens', async function () {
-      const totalSupply = await this.token.totalSupply();
+    const calculateGasCost = async (receipt) => {
+        const tx = await web3.eth.getTransaction(receipt.tx);
+        const gasPrice = tx.gasPrice;
+        return {
+            gasPrice: gasPrice,
+            gasUsed: receipt.receipt.gasUsed,
+            gasCost: gasPrice.mul(receipt.receipt.gasUsed)
+        };
+    };
 
-      assert.equal(totalSupply, 100);
-    });
-  });
+    const initialBalance = 100;
+    const gasReport = [];
 
-  describe('balanceOf', function () {
-    describe('when the requested account has no tokens', function () {
-      it('returns zero', async function () {
-        const balance = await this.token.balanceOf(anotherAccount);
-
-        assert.equal(balance, 0);
-      });
-    });
-
-    describe('when the requested account has some tokens', function () {
-      it('returns the total amount of tokens', async function () {
-        const balance = await this.token.balanceOf(owner);
-
-        assert.equal(balance, 100);
-      });
-    });
-  });
-
-  describe('transfer', function () {
-    describe('when the recipient is not the zero address', function () {
-      const to = recipient;
-
-      describe('when the sender does not have enough balance', function () {
-        const amount = 101;
-
-        it('reverts', async function () {
-          await assertRevert(this.token.transfer(to, amount, { from: owner }));
-        });
-      });
-
-      describe('when the sender has enough balance', function () {
-        const amount = 100;
-
-        it('transfers the requested amount', async function () {
-          await this.token.transfer(to, amount, { from: owner });
-
-          const senderBalance = await this.token.balanceOf(owner);
-          assert.equal(senderBalance, 0);
-
-          const recipientBalance = await this.token.balanceOf(to);
-          assert.equal(recipientBalance, amount);
-        });
-
-        it('emits a transfer event', async function () {
-          const { logs } = await this.token.transfer(to, amount, { from: owner });
-
-          assert.equal(logs.length, 1);
-          assert.equal(logs[0].event, 'Transfer');
-          assert.equal(logs[0].args.from, owner);
-          assert.equal(logs[0].args.to, to);
-          assert(logs[0].args.value.eq(amount));
-        });
-      });
+    beforeEach(async function () {
+        this.token = await WakERC20Token.new(owner, initialBalance);
     });
 
-    describe('when the recipient is the zero address', function () {
-      const to = ZERO_ADDRESS;
-
-      it('reverts', async function () {
-        await assertRevert(this.token.transfer(to, 100, { from: owner }));
-      });
-    });
-  });
-
-  describe('transfer from', function () {
-    const spender = recipient;
-
-    describe('when the recipient is not the zero address', function () {
-      const to = anotherAccount;
-
-      describe('when the spender has enough approved balance', function () {
-        beforeEach(async function () {
-          await this.token.approve(spender, 100, { from: owner });
+    describe('total supply', function () {
+        it('returns the total amount of tokens', async function () {
+            const totalSupply = await this.token.totalSupply();
+            assert.equal(totalSupply, 100);
         });
-
-        describe('when the owner has enough balance', function () {
-          const amount = 100;
-
-          it('transfers the requested amount', async function () {
-            await this.token.transferFrom(owner, to, amount, { from: spender });
-
-            const senderBalance = await this.token.balanceOf(owner);
-            assert.equal(senderBalance, 0);
-
-            const recipientBalance = await this.token.balanceOf(to);
-            assert.equal(recipientBalance, amount);
-          });
-
-          it('decreases the spender allowance', async function () {
-            await this.token.transferFrom(owner, to, amount, { from: spender });
-
-            const allowance = await this.token.allowance(owner, spender);
-            assert(allowance.eq(0));
-          });
-
-          it('emits a transfer event', async function () {
-            const { logs } = await this.token.transferFrom(owner, to, amount, { from: spender });
-
-            assert.equal(logs.length, 1);
-            assert.equal(logs[0].event, 'Transfer');
-            assert.equal(logs[0].args.from, owner);
-            assert.equal(logs[0].args.to, to);
-            assert(logs[0].args.value.eq(amount));
-          });
-        });
-
-        describe('when the owner does not have enough balance', function () {
-          const amount = 101;
-
-          it('reverts', async function () {
-            await assertRevert(this.token.transferFrom(owner, to, amount, { from: spender }));
-          });
-        });
-      });
-
-      describe('when the spender does not have enough approved balance', function () {
-        beforeEach(async function () {
-          await this.token.approve(spender, 99, { from: owner });
-        });
-
-        describe('when the owner has enough balance', function () {
-          const amount = 100;
-
-          it('reverts', async function () {
-            await assertRevert(this.token.transferFrom(owner, to, amount, { from: spender }));
-          });
-        });
-
-        describe('when the owner does not have enough balance', function () {
-          const amount = 101;
-
-          it('reverts', async function () {
-            await assertRevert(this.token.transferFrom(owner, to, amount, { from: spender }));
-          });
-        });
-      });
     });
 
-    describe('when the recipient is the zero address', function () {
-      const amount = 100;
-      const to = ZERO_ADDRESS;
+    describe('transfer', function () {
+        describe('when the recipient is not the zero address', function () {
+            const to = recipient;
 
-      beforeEach(async function () {
-        await this.token.approve(spender, amount, { from: owner });
-      });
+            describe('when the sender has enough balance', function () {
+                const amount = 100;
 
-      it('reverts', async function () {
-        await assertRevert(this.token.transferFrom(owner, to, amount, { from: spender }));
-      });
+                it('transfers the requested amount', async function () {
+                    const tx = await this.token.transfer(to, amount, {from: owner});
+                    const txGas = await calculateGasCost(tx);
+                    gasReport.push({msg: 'transfer', ...txGas});
+
+                    const senderBalance = await this.token.balanceOf(owner);
+                    assert.equal(senderBalance, 0);
+
+                    const recipientBalance = await this.token.balanceOf(to);
+                    assert.equal(recipientBalance, amount);
+                });
+
+                it('emits a transfer event', async function () {
+                    const {logs} = await this.token.transfer(to, amount, {from: owner});
+
+                    assert.equal(logs.length, 1);
+                    assert.equal(logs[0].event, 'Transfer');
+                    assert.equal(logs[0].args.from, owner);
+                    assert.equal(logs[0].args.to, to);
+                    assert(logs[0].args.value.eq(amount));
+                });
+            });
+        });
     });
-  });
 
+    describe('transfer multiple', function () {
+        describe('when the recipient is not the zero address', function () {
+            const to = recipient;
+
+            describe('when the sender has enough balance (single)', function () {
+                const amount = 100;
+
+                it('transfers the requested amount', async function () {
+                    const tx = await this.token.batchTransfer([to], [amount], {from: owner});
+                    const txGas = await calculateGasCost(tx);
+                    gasReport.push({msg: 'batchTransfer (1)', ...txGas});
+
+                    const senderBalance = await this.token.balanceOf(owner);
+                    assert.equal(senderBalance, 0);
+
+                    const recipientBalance = await this.token.balanceOf(to);
+                    assert.equal(recipientBalance, amount);
+                });
+
+                it('emits a transfer event', async function () {
+                    const {logs} = await this.token.batchTransfer([to], [amount], {from: owner});
+
+                    assert.equal(logs.length, 1);
+                    assert.equal(logs[0].event, 'Transfer');
+                    assert.equal(logs[0].args.from, owner);
+                    assert.equal(logs[0].args.to, to);
+                    assert(logs[0].args.value.eq(amount));
+                });
+            });
+
+            describe('when the sender has enough balance (multiple - 10)', function () {
+                const amount = 1;
+                const toAddresses = [
+                    account1,
+                    account2,
+                    account3,
+                    account4,
+                    account5,
+                    account6,
+                    account7,
+                    account8,
+                    account9,
+                    account10
+                ];
+                const amounts = toAddresses.map(() => amount);
+
+                it('transfers the requested amount', async function () {
+                    const tx = await this.token.batchTransfer(toAddresses, amounts, {from: owner});
+                    const txGas = await calculateGasCost(tx);
+                    gasReport.push({msg: 'batchTransfer (10)', ...txGas});
+
+                    const senderBalance = await this.token.balanceOf(owner);
+                    assert.equal(senderBalance, initialBalance - (amount * toAddresses.length));
+
+                    toAddresses.forEach(async (account) => {
+                        const balance = await this.token.balanceOf(account);
+                        assert.equal(balance, amount);
+                    });
+                });
+
+                it('emits a transfer event', async function () {
+                    const {logs} = await this.token.batchTransfer(toAddresses, amounts, {from: owner});
+
+                    assert.equal(logs.length, toAddresses.length);
+                    toAddresses.forEach(async (account, i) => {
+                        assert.equal(logs[i].event, 'Transfer');
+                        assert.equal(logs[i].args.from, owner);
+                        assert.equal(logs[i].args.to, account);
+                        assert(logs[i].args.value.eq(amount));
+                    });
+
+                });
+            });
+        });
+    });
+
+    after(async function() {
+
+        console.log('\n');
+
+        let table = new AsciiTable('WakERC20Token Gas Costs');
+        table.setHeading('function', 'gas price', 'gas user', 'gas cost');
+
+        gasReport.forEach((gasTx) => {
+            table.addRow(gasTx.msg, gasTx.gasPrice, gasTx.gasUsed, `${weiToEther(gasTx.gasCost)} ETH`)
+        });
+
+        console.log(table.toString())
+    });
 });
